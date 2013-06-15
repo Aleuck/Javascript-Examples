0b8e1b49ed93d6bd70ccfb9396d807876b3808bd
Alexandre Leuck <aleuck2004@gmail.com>
2013-06-15 15:17:41 -0300

    Update lambda-factorial.js
    Now using only 1 parameter lambda abstractions.
    Proper indentation and commends added.

--- lambda-factorial.js
+++ lambda-factorial.js
@@ -1,3 +1,14 @@
-var n = 5; // n!
-var r = (function (x) { return (function (f,x) { return f(f,x); }(function (f,x) { if(x <= 1) { return 1; } else { return x*f(f,x-1)} },x)); })(n);
-print(r);
+// Wrapping the whole function in parenthesis to make it a function expression, 
+// because function statements can't be anonymous.
+(function (x) {
+    return (function (g) {
+        return g(g)(x);
+    }(function (f) { // this is the function that is being passed as a parameter to the previous.
+        return function (y) {
+            if (y <= 1) {
+                return 1; // base case
+            }
+            return y * f(f)(y - 1) // recursion definition
+        }
+    }));
+} (5));
