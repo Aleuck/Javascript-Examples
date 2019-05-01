cffd16ba2556281e51dfeee878fcb59fe6d37d05
Alexandre Leuck <aleuck2004@gmail.com>
2019-05-01 18:02:00 -0300

    improving comments a bit

--- lambda-examples/factorial.js
+++ lambda-examples/factorial.js
@@ -2,11 +2,11 @@
 // Note that this function is extremely complex and hard to read. It is just a example for educational purposes.
 // You should never do something like this when developing applications
 
-// Wrapping the whole function in parenthesis to make it a function expression, 
-// because function statements can't be anonymous.
+// Wrapping the functions in parenthesis to make them function expressions, 
+// function statements can't be anonymous.
 (function (g) {
-    return g(g);
-})(function (f) { // this is the function that is being passed as a parameter to the previous.
+    return g(g); // fixed point function
+})(function (f) {
     return function (x) {
         if (x <= 1) {
             return 1; // base case
